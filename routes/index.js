73216b9f333d31fb71ecd21b25ead76ab47d6e07
flodavis <fpdavis86@gmail.com>
2015-10-23 16:28:22 -0700

    Adding code for routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,71 @@
 var express = require('express');
+var Movie=require('../models/movie');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.route('/movies')
+    .get(function(req,res){
+       Movie.find(function(err,movies){
+           if(err)
+                res.send(err);
+           res.json(movies);
+       });
+    })
+
+    .post(function(req,res){
+        var movie=new Movie(req.body);
+        movie.save(function(err){
+            if(err)
+                res.send(err);
+            res.send({message:'Movie Added'});
+        });
+    });
+
+router.route('/movies/:id')
+    .put(function(req,res){
+        Movie.findOne({_id:req.params.id},function(err,movie){
+
+            if(err)
+                res.send(err);
+
+           for(prop in req.body){
+                movie[prop]=req.body[prop];
+           }
+
+            // save the movie
+            movie.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Movie updated!' });
+            });
+
+        });
+    })
+
+    .get(function(req,res){
+        Movie.findOne({_id:req.params.id},function(err, movie) {
+            if(err)
+                res.send(err);
+
+            res.json(movie);
+        });
+    })
+
+    .delete(function(req,res){
+        Movie.remove({
+            _id: req.params.id
+        }, function(err, movie) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
 
 module.exports = router;
